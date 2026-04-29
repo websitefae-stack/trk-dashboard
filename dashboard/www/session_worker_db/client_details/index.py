92fcf8eb234166fb43b443a0691a2696bacd33b2
websitefae-stack <websitefae@gmail.com>
2026-04-29 12:29:05 +0200

    Create index.py

---
+++ dashboard/www/session_worker_db/client_details/index.py
@@ -0,0 +1,34 @@
+import frappe
+from frappe import _
+from dashboard.api.session_worker.client_details import (
+    get_client_for_context,
+    get_session_worker_name,
+)
+
+
+def get_context(context):
+    if frappe.session.user == "Guest":
+        frappe.throw(_("Login required"), frappe.PermissionError)
+
+    context.no_cache = 1
+    context.page_title = "Client Details"
+    context.active_page = "clients"
+
+    context.dashboard_notifications_url = "/session_worker_db/notifications"
+
+    try:
+        context.dashboard_user_name = get_session_worker_name()
+        context.session_worker_name = context.dashboard_user_name
+    except Exception:
+        context.dashboard_user_name = frappe.get_cached_value("User", frappe.session.user, "full_name") or frappe.session.user
+        context.session_worker_name = context.dashboard_user_name
+
+    client_name = frappe.form_dict.get("name")
+    if not client_name:
+        frappe.throw(_("Client not found."))
+
+    client = get_client_for_context(client_name)
+
+    context.client_docname = client.name
+    context.client_title = client.get("full_name") or client.name
+    context.client = client.as_dict()
