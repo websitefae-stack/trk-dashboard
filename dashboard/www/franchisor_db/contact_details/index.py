0e742618973b839d815c52e745a90d0002dc8408
websitefae-stack <websitefae@gmail.com>
2026-04-29 14:39:28 +0200

    Refactor contact details to use client context data

--- dashboard/www/franchisor_db/contact_details/index.py
+++ dashboard/www/franchisor_db/contact_details/index.py
@@ -1,22 +1,28 @@
 import frappe
 from frappe import _
-from dashboard.api.shared.contact_details import get_contact_context
+from dashboard.api.franchisor.clients import get_franchisor_display_name
+from dashboard.api.coach.client_details import get_client_context_data
 
 
 def get_context(context):
     if frappe.session.user == "Guest":
         frappe.throw(_("Login required"), frappe.PermissionError)
 
-    data = get_contact_context(
-        scope="franchisor",
-        contact_name=frappe.form_dict.get("name"),
-        is_new=bool(frappe.form_dict.get("new")),
-    )
-
     context.no_cache = 1
-    context.page_title = data["contact_display_name"]
-    context.active_page = "contacts"
+    context.page_title = "Client Details"
+    context.active_page = "clients"
+
+    context.dashboard_user_name = get_franchisor_display_name()
     context.dashboard_notifications_url = "/franchisor_db/notifications"
+
+    client_name = frappe.form_dict.get("name")
+    is_new = frappe.form_dict.get("new")
+
+    data = get_client_context_data(
+        client_name=client_name,
+        is_new=bool(is_new),
+        base_url="/franchisor_db",
+    )
 
     for key, value in data.items():
         context[key] = value
