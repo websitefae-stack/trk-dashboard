4b8eba3908a3176b7fa3d5819c021532580da87d
websitefae-stack <websitefae@gmail.com>
2026-04-29 18:33:00 +0200

    Update index.py

--- dashboard/www/session_worker_db/contact_details/index.py
+++ dashboard/www/session_worker_db/contact_details/index.py
@@ -1,11 +1,15 @@
 import frappe
 from frappe import _
+
+from dashboard.api.shared.permissions import redirect_if_wrong_dashboard
 from dashboard.api.shared.contact_details import get_contact_context
 
 
 def get_context(context):
     if frappe.session.user == "Guest":
         frappe.throw(_("Login required"), frappe.PermissionError)
+
+    redirect_if_wrong_dashboard("session_worker")
 
     data = get_contact_context(
         scope="session_worker",
