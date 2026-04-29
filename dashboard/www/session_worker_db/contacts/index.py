31b2539d728147392250d0bc523d16c2fcd1f629
websitefae-stack <websitefae@gmail.com>
2026-04-29 18:32:44 +0200

    Add dashboard redirection check for session worker

--- dashboard/www/session_worker_db/contacts/index.py
+++ dashboard/www/session_worker_db/contacts/index.py
@@ -1,11 +1,15 @@
 import frappe
 from frappe import _
+
+from dashboard.api.shared.permissions import redirect_if_wrong_dashboard
 from dashboard.api.shared.contacts import get_contacts_for_scope
 
 
 def get_context(context):
     if frappe.session.user == "Guest":
         frappe.throw(_("Login required"), frappe.PermissionError)
+
+    redirect_if_wrong_dashboard("session_worker")
 
     context.no_cache = 1
     context.page_title = "Contacts"
