25f69fb7b328c569e9c63e941e2bb71947055b6a
websitefae-stack <websitefae@gmail.com>
2026-04-29 13:23:14 +0200

    Create index.py

---
+++ dashboard/www/coach_db/client_details/index.py
@@ -0,0 +1,30 @@
+import frappe
+from frappe import _
+from dashboard.api.coach.client_details import (
+    get_client_context_data,
+    get_coach_name,
+)
+
+
+def get_context(context):
+    if frappe.session.user == "Guest":
+        frappe.throw(_("Login required"), frappe.PermissionError)
+
+    context.no_cache = 1
+    context.page_title = "Client Details"
+    context.active_page = "clients"
+
+    context.dashboard_user_name = get_coach_name()
+    context.dashboard_notifications_url = "/coach_db/notifications"
+
+    client_name = frappe.form_dict.get("name")
+    is_new = frappe.form_dict.get("new")
+
+    data = get_client_context_data(
+        client_name=client_name,
+        is_new=bool(is_new),
+        base_url="/coach_db",
+    )
+
+    for key, value in data.items():
+        context[key] = value
