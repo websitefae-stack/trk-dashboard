0ff88569b733c568bc5255cea3a725b8463d91df
websitefae-stack <websitefae@gmail.com>
2026-04-29 14:28:03 +0200

    Create index.py

---
+++ dashboard/www/session_worker_db/contact_details/index.py
@@ -0,0 +1,22 @@
+import frappe
+from frappe import _
+from dashboard.api.shared.contact_details import get_contact_context
+
+
+def get_context(context):
+    if frappe.session.user == "Guest":
+        frappe.throw(_("Login required"), frappe.PermissionError)
+
+    data = get_contact_context(
+        scope="session_worker",
+        contact_name=frappe.form_dict.get("name"),
+        is_new=False,
+    )
+
+    context.no_cache = 1
+    context.page_title = data["contact_display_name"]
+    context.active_page = "contacts"
+    context.dashboard_notifications_url = "/session_worker_db/notifications"
+
+    for key, value in data.items():
+        context[key] = value
