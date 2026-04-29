f9a7ab1ed964928f80149fb2457a695123328cec
websitefae-stack <websitefae@gmail.com>
2026-04-29 18:30:59 +0200

    Update index.py

--- dashboard/www/session_worker_db/index.py
+++ dashboard/www/session_worker_db/index.py
@@ -3,9 +3,12 @@ from frappe import _
 
 from dashboard.api.shared.permissions import redirect_if_wrong_dashboard
 
+
 def get_context(context):
     if frappe.session.user == "Guest":
         frappe.throw(_("Login required"), frappe.PermissionError)
+
+    redirect_if_wrong_dashboard("session_worker")
 
     context.no_cache = 1
     context.page_title = "Session Worker Dashboard"
