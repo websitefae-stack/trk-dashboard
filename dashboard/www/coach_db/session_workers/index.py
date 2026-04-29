baf7eec66f924eb85d8ee6e8efcb613e70d39be8
websitefae-stack <websitefae@gmail.com>
2026-04-29 17:53:08 +0200

    Add session workers context retrieval function

---
+++ dashboard/www/coach_db/session_workers/index.py
@@ -0,0 +1,20 @@
+import frappe
+from frappe import _
+
+from dashboard.api.coach.profile import get_coach_display_name
+from dashboard.api.coach.session_workers import get_linked_session_workers
+
+
+def get_context(context):
+    if frappe.session.user == "Guest":
+        frappe.throw(_("Login required"), frappe.PermissionError)
+
+    context.no_cache = 1
+    context.page_title = "Session Workers"
+    context.active_page = "session_workers"
+
+    context.dashboard_user_name = get_coach_display_name()
+    context.dashboard_notifications_url = "/coach_db/notifications"
+
+    context.session_workers = get_linked_session_workers()
+    context.session_worker_detail_base_url = "/coach_db/session_worker_details"
