0a17d5b5749878a4c760838aadfa06328ce36b7d
websitefae-stack <websitefae@gmail.com>
2026-04-29 17:34:09 +0200

    Create notifications.py

---
+++ dashboard/api/franchisor/notifications.py
@@ -0,0 +1,9 @@
+from dashboard.api.shared.notifications import (
+    get_notifications,
+    get_notification_detail,
+    update_notification_status,
+    get_dashboard_notification_summary,
+    get_notification_list_for_page,
+    get_notification_summary_for_page,
+    ensure_notification_access,
+)
