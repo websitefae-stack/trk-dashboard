b5c3c59c3bd02ab7b500be54b099d8ebedee7d71
websitefae-stack <websitefae@gmail.com>
2026-04-29 18:46:24 +0200

    Update index.py

--- dashboard/www/coach_db/index.py
+++ dashboard/www/coach_db/index.py
@@ -1,11 +1,15 @@
 import frappe
 from frappe import _
-from dashboard.api.coach.clients import get_coach_display_name
+
+from dashboard.api.shared.permissions import redirect_if_wrong_dashboard
+from dashboard.api.coach.profile import get_coach_display_name
 
 
 def get_context(context):
     if frappe.session.user == "Guest":
         frappe.throw(_("Login required"), frappe.PermissionError)
+
+    redirect_if_wrong_dashboard("coach")
 
     context.no_cache = 1
     context.page_title = "Coach Dashboard"
