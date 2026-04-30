640d5f64c07fa6a74a8ab8ba27c5eb9a64b8be28
websitefae-stack <websitefae@gmail.com>
2026-04-30 09:45:52 +0200

    Update index.py

--- dashboard/www/franchisor_db/contacts/index.py
+++ dashboard/www/franchisor_db/contacts/index.py
@@ -2,7 +2,8 @@ import frappe
 from frappe import _
 
 from dashboard.api.shared.permissions import redirect_if_wrong_dashboard
-from dashboard.api.shared.contacts import get_contacts_for_scope
+from dashboard.api.shared.contacts import get_contacts_for_scope, get_current_coach_name
+from dashboard.api.franchisor.clients import get_coaches
 
 
 def get_context(context):
@@ -11,10 +12,21 @@ def get_context(context):
 
     redirect_if_wrong_dashboard("franchisor")
 
-    show_all = frappe.form_dict.get("show_all") == "1"
+    contact_scope = frappe.form_dict.get("contact_scope") or "my"
 
     context.no_cache = 1
     context.page_title = "Contacts"
     context.active_page = "contacts"
-    context.show_all = show_all
-    context.contacts = get_contacts_for_scope("franchisor", show_all=show_all)
+    context.contact_scope = contact_scope
+    context.my_coach_name = get_current_coach_name()
+    context.my_coach_display_name = frappe.db.get_value(
+        "Coach",
+        context.my_coach_name,
+        "coach_name",
+    ) or "My contacts"
+    context.coaches = get_coaches()
+    context.contacts = get_contacts_for_scope(
+        "franchisor",
+        show_all=False,
+        coach_scope=contact_scope,
+    )
