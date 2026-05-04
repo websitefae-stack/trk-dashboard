b0eab9539db69bf24e3129c253305126550a2e82
websitefae-stack <websitefae@gmail.com>
2026-05-04 09:53:06 +0200

    Update index.py

--- dashboard/www/coach_db/contacts/index.py
+++ dashboard/www/coach_db/contacts/index.py
@@ -2,7 +2,7 @@ import frappe
 from frappe import _
 
 from dashboard.api.shared.permissions import redirect_if_wrong_dashboard
-from dashboard.api.shared.contacts import get_contacts_for_scope
+from dashboard.api.shared.contacts import get_contacts_for_scope, get_current_coach_name
 
 
 def get_context(context):
@@ -14,4 +14,9 @@ def get_context(context):
     context.no_cache = 1
     context.page_title = "Contacts"
     context.active_page = "contacts"
+    context.dashboard_user_name = frappe.db.get_value(
+        "Coach",
+        get_current_coach_name(),
+        "coach_name",
+    ) or frappe.session.user
     context.contacts = get_contacts_for_scope("coach")
