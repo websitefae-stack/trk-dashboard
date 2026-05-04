f62a062c31b9e724156b020c8198bffa7deeab46
websitefae-stack <websitefae@gmail.com>
2026-05-04 20:53:47 +0200

    Update index.py

--- dashboard/www/franchisor_db/calendar/index.py
+++ dashboard/www/franchisor_db/calendar/index.py
@@ -1,1 +1,13 @@
+import frappe
+from frappe import _
+from frappe.utils import get_fullname
 
+
+def get_context(context):
+    if frappe.session.user == "Guest":
+        frappe.throw(_("Login required"), frappe.PermissionError)
+
+    context.no_cache = 1
+    context.active_page = "calendar"
+    context.dashboard_notifications_url = "/franchisor_db/notifications"
+    context.dashboard_user_name = get_fullname(frappe.session.user)
