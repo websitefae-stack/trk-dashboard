2203b3924d7b00bb0b1eb911c0cc4f17b51247c1
websitefae-stack <websitefae@gmail.com>
2026-05-04 13:43:15 +0200

    Update index.py

--- dashboard/www/session_worker_db/calendar_details/index.py
+++ dashboard/www/session_worker_db/calendar_details/index.py
@@ -1,1 +1,11 @@
+import frappe
+from frappe import _
 
+
+def get_context(context):
+    if frappe.session.user == "Guest":
+        frappe.throw(_("Login required"), frappe.PermissionError)
+
+    context.no_cache = 1
+    context.active_page = "calendar"
+    context.dashboard_notifications_url = "/session_worker_db/notifications"
