08e9481009d57e1d98681a3f1a173fc4cddaae9e
websitefae-stack <websitefae@gmail.com>
2026-05-04 09:26:35 +0200

    Update index.py

--- dashboard/www/franchisor_db/contact_details/index.py
+++ dashboard/www/franchisor_db/contact_details/index.py
@@ -2,8 +2,8 @@ import frappe
 from frappe import _
 
 from dashboard.api.shared.permissions import redirect_if_wrong_dashboard
+from dashboard.api.shared.contact_details import get_contact_context
 from dashboard.api.franchisor.clients import get_franchisor_display_name
-from dashboard.api.coach.client_details import get_client_context_data
 
 
 def get_context(context):
@@ -12,21 +12,19 @@ def get_context(context):
 
     redirect_if_wrong_dashboard("franchisor")
 
-    context.no_cache = 1
-    context.page_title = "Client Details"
-    context.active_page = "clients"
+    is_new = bool(frappe.form_dict.get("new"))
 
+    data = get_contact_context(
+        scope="franchisor",
+        contact_name=frappe.form_dict.get("name"),
+        is_new=is_new,
+    )
+
+    context.no_cache = 1
+    context.page_title = data["contact_display_name"]
+    context.active_page = "contacts"
     context.dashboard_user_name = get_franchisor_display_name()
     context.dashboard_notifications_url = "/franchisor_db/notifications"
-
-    client_name = frappe.form_dict.get("name")
-    is_new = frappe.form_dict.get("new")
-
-    data = get_client_context_data(
-        client_name=client_name,
-        is_new=bool(is_new),
-        base_url="/franchisor_db",
-    )
 
     for key, value in data.items():
         context[key] = value
