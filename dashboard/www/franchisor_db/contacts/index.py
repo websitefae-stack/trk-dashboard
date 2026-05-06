f09515bb4d6be23b99a6dcba5168cf6136e2c365
websitefae-stack <websitefae@gmail.com>
2026-05-06 17:48:14 +0200

    Update index.py

--- dashboard/www/franchisor_db/contacts/index.py
+++ dashboard/www/franchisor_db/contacts/index.py
@@ -2,8 +2,37 @@ import frappe
 from frappe import _
 
 from dashboard.api.shared.permissions import redirect_if_wrong_dashboard
-from dashboard.api.shared.contacts import get_contacts_for_scope, get_current_coach_name
-from dashboard.api.franchisor.clients import get_coaches, get_session_workers, get_franchisor_display_name
+from dashboard.api.shared.contacts import (
+    get_contacts_for_scope,
+    get_current_coach_name,
+)
+
+
+def get_coaches():
+    return frappe.get_all(
+        "Coach",
+        fields=["name", "coach_name"],
+        order_by="coach_name asc",
+    )
+
+
+def get_session_workers():
+    return frappe.get_all(
+        "Session Worker",
+        fields=["name"],
+        order_by="name asc",
+    )
+
+
+def get_franchisor_display_name():
+    return (
+        frappe.db.get_value(
+            "Coach",
+            {"user": frappe.session.user},
+            "coach_name",
+        )
+        or frappe.session.user
+    )
 
 
 def get_context(context):
@@ -17,16 +46,23 @@ def get_context(context):
     context.no_cache = 1
     context.page_title = "Contacts"
     context.active_page = "contacts"
+
     context.dashboard_user_name = get_franchisor_display_name()
+
     context.contact_scope = contact_scope
+
     context.my_coach_name = get_current_coach_name()
+
     context.my_coach_display_name = frappe.db.get_value(
         "Coach",
         context.my_coach_name,
         "coach_name",
     ) or "My contacts"
+
     context.coaches = get_coaches()
+
     context.session_workers = get_session_workers()
+
     context.contacts = get_contacts_for_scope(
         "franchisor",
         show_all=False,
