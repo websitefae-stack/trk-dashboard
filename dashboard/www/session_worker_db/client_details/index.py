8a30d264839ff221cadaebda66021f50f7438ea8
websitefae-stack <websitefae@gmail.com>
2026-05-06 07:29:48 +0200

    Update index.py

--- dashboard/www/session_worker_db/client_details/index.py
+++ dashboard/www/session_worker_db/client_details/index.py
@@ -3,8 +3,8 @@ from frappe import _
 
 from dashboard.api.shared.permissions import redirect_if_wrong_dashboard
 from dashboard.api.shared.client_details import (
-    get_client_context_data,
-    get_coach_name,
+    get_client_for_context,
+    get_session_worker_name,
 )
 
 
@@ -12,31 +12,28 @@ def get_context(context):
     if frappe.session.user == "Guest":
         frappe.throw(_("Login required"), frappe.PermissionError)
 
-    redirect_if_wrong_dashboard("coach")
+    redirect_if_wrong_dashboard("session_worker")
 
     context.no_cache = 1
     context.page_title = "Client Details"
     context.active_page = "clients"
-    context.dashboard_notifications_url = "/coach_db/notifications"
-    context.dashboard_user_name = get_coach_name()
+    context.dashboard_notifications_url = "/session_worker_db/notifications"
+    context.dashboard_user_name = get_session_worker_name()
 
-    context.client_details_role = "coach"
-    context.client_details_base_url = "/coach_db"
+    context.client_details_role = "session_worker"
+    context.client_details_base_url = "/session_worker_db"
     context.client_details_api_base = "dashboard.api.shared.client_details"
-    context.client_details_storage_key = "coach_client_details_active_tab"
-    context.client_details_can_edit = 1
-    context.client_details_can_invoice = 1
-    context.client_details_can_request_change = 0
+    context.client_details_storage_key = "session_worker_client_details_active_tab"
+    context.client_details_can_edit = 0
+    context.client_details_can_invoice = 0
+    context.client_details_can_request_change = 1
 
     client_name = frappe.form_dict.get("name")
-    is_new = frappe.form_dict.get("new")
+    if not client_name:
+        frappe.throw(_("Client not found."))
 
-    data = get_client_context_data(
-        client_name=client_name,
-        is_new=bool(is_new),
-        base_url="/coach_db",
-        enforce_access=True,
-    )
+    client = get_client_for_context(client_name)
 
-    for key, value in data.items():
-        context[key] = value
+    context.client = client.as_dict()
+    context.client_docname = client.name
+    context.client_title = client.get("full_name") or client.name
