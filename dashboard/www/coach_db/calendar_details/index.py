1c22280b044fea89835295e36008a796841e1de5
websitefae-stack <websitefae@gmail.com>
2026-05-07 07:13:57 +0200

    Update index.py

--- dashboard/www/coach_db/calendar_details/index.py
+++ dashboard/www/coach_db/calendar_details/index.py
@@ -9,6 +9,6 @@ def get_context(context):
 
     context.no_cache = 1
     context.active_page = "calendar"
-    context.dashboard_type = "coach"
-    context.dashboard_notifications_url = "/coach_db/notifications"
+    context.dashboard_type = "franchisor"
+    context.dashboard_notifications_url = "/franchisor_db/notifications"
     context.dashboard_user_name = get_fullname(frappe.session.user)
