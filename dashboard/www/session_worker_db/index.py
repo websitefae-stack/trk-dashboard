9a25752d710112a5a2174f3db17ebc2e33d2a375
websitefae-stack <websitefae@gmail.com>
2026-05-08 21:43:47 +0200

    Update index.py

--- dashboard/www/session_worker_db/index.py
+++ dashboard/www/session_worker_db/index.py
@@ -13,5 +13,4 @@ def get_context(context):
     context.no_cache = 1
     context.page_title = "Session Worker Dashboard"
     context.active_page = "dashboard"
-    context.dashboard_notifications_url = "/session_worker_db/notifications"
     context.dashboard_user_name = frappe.get_cached_value("User", frappe.session.user, "full_name") or frappe.session.user
