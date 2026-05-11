8c5efb6517bbf468098a190c3e0468f2d011ca5c
websitefae-stack <websitefae@gmail.com>
2026-05-11 12:09:21 +0200

    Delete dashboard/api/session_worker/OLD-client_details.py

--- dashboard/api/session_worker/OLD-client_details.py
+++
@@ -1,8 +0,0 @@
-from dashboard.api.shared.client_details import (
-    add_client_note,
-    get_client_contacts,
-    get_client_notes,
-    get_client_appointments,
-    get_client_for_context,
-    get_session_worker_name,
-)
