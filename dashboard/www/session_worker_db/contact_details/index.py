a88bd3ade594d0880da07c1dd53aad21f23be3eb
websitefae-stack <websitefae@gmail.com>
2026-05-13 22:14:19 +0200

    Update index.py

--- dashboard/www/session_worker_db/contact_details/index.py
+++ dashboard/www/session_worker_db/contact_details/index.py
@@ -1,15 +1,21 @@
 import frappe
 from frappe import _
 
-from dashboard.api.shared.permissions import redirect_if_wrong_dashboard
 from dashboard.api.shared.contact_details import get_contact_context
+from dashboard.api.shared.session_worker_view_mode import get_session_worker_view_mode
 
 
 def get_context(context):
     if frappe.session.user == "Guest":
         frappe.throw(_("Login required"), frappe.PermissionError)
 
-    redirect_if_wrong_dashboard("session_worker")
+    view_as = frappe.form_dict.get("view_as")
+    viewer = frappe.form_dict.get("viewer")
+
+    view_mode = get_session_worker_view_mode(
+        scope=viewer,
+        worker_name=view_as,
+    )
 
     data = get_contact_context(
         scope="session_worker",
@@ -20,12 +26,22 @@ def get_context(context):
     context.no_cache = 1
     context.page_title = data["contact_display_name"]
     context.active_page = "contacts"
-    context.dashboard_user_name = frappe.db.get_value(
-        "Session Worker",
-        {"user": frappe.session.user},
-        "sw_name",
-    ) or frappe.session.user
     context.dashboard_notifications_url = "/session_worker_db/notifications"
+
+    context.session_worker_view_mode = view_mode
+    context.session_worker_view_query = view_mode.get("query_string") or ""
+    context.session_worker_is_view_mode = view_mode.get("is_view_mode") or 0
+    context.session_worker_view_return_to = view_mode.get("return_to") or ""
+    context.session_worker_view_display_name = view_mode.get("view_worker_display_name") or ""
+
+    if context.session_worker_is_view_mode:
+        context.dashboard_user_name = context.session_worker_view_display_name
+    else:
+        context.dashboard_user_name = frappe.db.get_value(
+            "Session Worker",
+            {"user": frappe.session.user},
+            "sw_name",
+        ) or frappe.session.user
 
     for key, value in data.items():
         context[key] = value
