45f012827b59ed554583eef6e3a8382f7158f632
websitefae-stack <websitefae@gmail.com>
2026-06-08 14:31:06 +0200

    Update index.py

--- dashboard/www/coach_db/calendar/index.py
+++ dashboard/www/coach_db/calendar/index.py
@@ -21,18 +21,18 @@ def get_context(context):
     context.no_cache = 1
     context.active_page = "calendar"
     context.dashboard_type = "coach"
-    context.dashboard_notifications_url = "/coach_db/notifications"
+    context.dashboard_notifications_url = "/coach_db/notifications" + (view_mode.get("query_string") or "")
+    context.dashboard_base_url = "/coach_db"
 
     context.coach_view_mode = view_mode
     context.coach_view_query = view_mode.get("query_string") or ""
     context.coach_is_view_mode = view_mode.get("is_view_mode") or 0
     context.coach_view_return_to = view_mode.get("return_to") or ""
     context.coach_view_display_name = view_mode.get("view_coach_display_name") or ""
+    context.view_as_coach = view_mode.get("view_coach_name") if view_mode.get("is_view_mode") else ""
 
     if context.coach_is_view_mode:
         context.dashboard_user_name = context.coach_view_display_name
-        context.view_as_coach = view_mode.get("view_coach_name")
     else:
         redirect_if_wrong_dashboard("coach")
         context.dashboard_user_name = get_fullname(frappe.session.user)
-        context.view_as_coach = ""
