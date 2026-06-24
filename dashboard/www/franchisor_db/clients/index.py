9df4ae7937388b3de15ba17e975d1fb1f399e418
websitefae-stack <websitefae@gmail.com>
2026-06-24 22:10:08 +0200

    Update index.py

--- dashboard/www/franchisor_db/clients/index.py
+++ dashboard/www/franchisor_db/clients/index.py
@@ -2,7 +2,7 @@ import frappe
 from frappe import _
 
 from dashboard.api.shared.permissions import redirect_if_wrong_dashboard
-from dashboard.api.shared.clients import get_clients, get_client_types
+from dashboard.api.shared.clients import get_paginated_clients, get_client_types
 from dashboard.api.shared.directory import (
     get_franchisor_display_name,
     get_session_workers,
@@ -22,7 +22,11 @@ def get_context(context):
     context.dashboard_notifications_url = "/franchisor_db/notifications"
     context.dashboard_user_name = get_franchisor_display_name()
 
-    context.clients = get_clients()
+    client_data = get_paginated_clients()
+
+    context.clients = client_data.get("clients", [])
+    context.pagination = client_data.get("pagination", {})
+    context.search = client_data.get("search", "")
     context.client_types = get_client_types()
     context.session_workers = get_session_workers()
     context.coaches = get_coaches()
